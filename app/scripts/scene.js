dca9b0563c812f5253480733904a99ef862f752b
M3tiii <m1995g@gmail.com>
2017-05-25 23:23:30 +0200

    Add changeable scene.

--- app/scripts/scene.js
+++ app/scripts/scene.js
@@ -4,21 +4,21 @@ import Digital from './digital';
 export default class Scene {
   constructor() {
 
-    this.visibleSettings = false;
+    this.actual = "analog";
 
-    let analog = new Analog();
-    analog.render();
-
-    let digital = new Digital();
-    digital.render();
+    this.clocks = {
+      analog: new Analog(),
+      digital: new Digital()
+    };
+    this.clocks.analog.render();
+    this.clocks.digital.render();
   }
 
-  openSettings() {
-
+  setSettings(name, action, value) {
+    this.clocks[this.actual].set(name, action, value);
   }
 
-  toggleSettings() {
-    this.visibleSettings = !this.visibleSettings;
-    console.log(this.visibleSettings);
+  toggleClock(name) {
+    this.actual = name;
   }
 }
