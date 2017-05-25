5354f2035861e78f63515d8cb15cd2dd6c30592a
M3tiii <m1995g@gmail.com>
2017-05-25 23:21:17 +0200

    Add show, hide function.

--- app/scripts/d3Service.js
+++ app/scripts/d3Service.js
@@ -3,6 +3,14 @@ import * as d3 from "d3";
 
 export default class d3Service {
 
+  hide(element) {
+    element.attr("visibility", "hidden");
+  }
+
+  show(element) {
+    element.attr("visibility", "visible");
+  }
+
   updateProperties(data, element) {
     for (let property in data[0]) {
       element.attr(property, function(d) {
