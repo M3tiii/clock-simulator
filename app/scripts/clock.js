9a3bee2f442d78a23237393d8e83bb609a21a0e1
M3tiii <m1995g@gmail.com>
2017-05-26 12:29:32 +0200

    Set actual date.

--- app/scripts/clock.js
+++ app/scripts/clock.js
@@ -1,6 +1,21 @@
 export default class Clock {
 
-  constructor() {}
+  constructor() {
+    this.actualDay = {};
+    this.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    this.months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+  }
+
+  set(name, action, value) {
+    switch (action) {
+      case "toggle-visibility":
+        value ? this.d3Service.show(this[name]) : this.d3Service.hide(this[name]);
+        break;
+      case "toggle-boolean":
+        this[name] = value;
+        break;
+    }
+  }
 
   getTime() {
     const currentdate = new Date();
@@ -15,14 +30,29 @@ export default class Clock {
   getDate() {
     const currentdate = new Date();
     return {
-      year: currentdate.getFullYear,
-      month: currentdate.getMonth + 1,
-      day: currentdate.getDate()
+      year: currentdate.getFullYear(),
+      month: currentdate.getMonth(),
+      day: currentdate.getDate(),
+      weekDay: currentdate.getDay()
+    }
+  }
+
+  fillDate() {
+    const actual = this.getDate();
+    if (JSON.stringify(actual) !== JSON.stringify(this.actualDay)) {
+      this.actualDay = actual;
+      let dayNamePanel = $(".day-name");
+      let dayDatePanel = $(".day-date");
+      const dayNameText = this.days[this.actualDay.weekDay - 1] + ',';
+      const dayDateText = `${this.actualDay.day} ${this.months[this.actualDay.month]} ${this.actualDay.year}`;
+      dayNamePanel.text(dayNameText);
+      dayDatePanel.text(dayDateText);
     }
   }
 
   run(callback) {
     setInterval(() => {
+      this.fillDate();
       callback();
     }, 1000);
   }
