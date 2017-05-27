3a19346f932432b90eef10d565705803e8100422
M3tiii <m1995g@gmail.com>
2017-05-27 02:04:04 +0200

    Fix spread operator.

--- app/scripts/scene.js
+++ app/scripts/scene.js
@@ -1,24 +1,29 @@
-import Analog from './analog';
-import Digital from './digital';
+import Analog from "./analog";
+import Digital from "./digital";
 
 export default class Scene {
   constructor() {
 
-    this.actual = "analog";
+    this.actual = "analog"; // actual selected clock
 
     this.clocks = {
       analog: new Analog(),
       digital: new Digital()
     };
+
     this.clocks.analog.render();
-    this.clocks.digital.render();
   }
 
-  setSettings(name, action, value) {
-    this.clocks[this.actual].set(name, action, value);
+  // forward event to actual clock handling checkboxes
+  setSettings(...args) {
+    this.clocks[this.actual].set(...args);
   }
 
+  // changing running clock
+  // stop last clock and start actual clock
   toggleClock(name) {
+    clearInterval(this.clocks[this.actual].intervalId);
+    this.clocks[name].render();
     this.actual = name;
   }
 }
