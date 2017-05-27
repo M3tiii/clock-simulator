fcb4844e4e0449adf02f84ed0332f6608f437767
M3tiii <m1995g@gmail.com>
2017-05-27 02:06:40 +0200

    Fix date render.

--- app/scripts/analog.js
+++ app/scripts/analog.js
@@ -1,13 +1,14 @@
-import Clock from './clock';
+import Clock from "./clock";
 import * as d3 from "d3";
-import d3Service from './d3Service';
+import d3Service from "./d3Service";
 
 export default class Analog extends Clock {
 
   constructor() {
     super();
 
-    this.d3Service = new d3Service();
+    const svgWidth = 300;
+    const svgHeight = 300;
     this.range = 100;
     this.center = 150;
     this.shortWidth = 8;
@@ -16,6 +17,14 @@ export default class Analog extends Clock {
     this.longHeight = 80;
     this.secondWidth = 2;
     this.secondHeight = 80;
+    this.fontSize = 18;
+    this.rounded = 5;
+    this.opacity = 0.85;
+
+    this.symbolsSign = {
+      arabic: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+      roman: ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']
+    };
 
     const borderData = [{
       cx: this.center,
@@ -27,84 +36,151 @@ export default class Analog extends Clock {
     const clockData = [{
       x: this.center - this.shortWidth / 2,
       y: this.center - this.shortWidth / 2,
+      rx: this.rounded,
       width: this.shortWidth,
       height: this.shortHeight,
-      class: "short-hand"
+      "fill-opacity": this.opacity,
+      "class": "short-hand"
     }, {
       x: this.center - this.longWidth / 2,
       y: this.center - this.longWidth / 2,
+      rx: this.rounded,
       width: this.longWidth,
       height: this.longHeight,
-      class: "long-hand"
+      "fill-opacity": this.opacity,
+      "class": "long-hand"
     }, {
       x: this.center - this.secondWidth / 2,
       y: this.center - this.secondWidth / 2,
+      rx: this.rounded,
       width: this.secondWidth,
       height: this.secondHeight,
-      class: "second-hand"
+      "fill-opacity": this.opacity,
+      "class": "second-hand"
+    }];
+
+    const dateData = [{
+      x: this.center,
+      y: this.center + this.range / 2,
+      "font-size": this.fontSize + "px",
+      "class": "digital-number analog-date",
+      "text-anchor": "middle",
+      "fill": "gray"
+    }, {
+      x: this.center,
+      y: this.center + this.range / 2 + this.fontSize * 1.1,
+      "font-size": this.fontSize + "px",
+      "class": "digital-number analog-day",
+      "text-anchor": "middle",
+      "fill": "gray"
     }];
 
     this.svgContainer = d3.select("div.clock-analog-svg").append("svg")
-      .attr("width", 300)
-      .attr("height", 300);
+      .attr("width", svgWidth)
+      .attr("height", svgHeight);
 
-    let border = this.svgContainer.selectAll("circle")
+    const date = this.svgContainer.selectAll("text")
+      .data(dateData)
+      .enter()
+      .append("text");
+
+    d3Service.updateProperties(dateData, date);
+
+    const border = this.svgContainer.selectAll("circle")
       .data(borderData)
       .enter()
       .append("circle");
 
-    this.d3Service.updateProperties(borderData, border);
+    d3Service.updateProperties(borderData, border);
 
-    let clocks = this.svgContainer.selectAll("rect")
+    const clocks = this.svgContainer.selectAll("rect")
       .data(clockData)
       .enter()
       .append("rect");
 
-    this.d3Service.updateProperties(clockData, clocks);
+    d3Service.updateProperties(clockData, clocks);
 
-    this.lastTime = {};
     this.short = clocks.filter(".short-hand");
     this.long = clocks.filter(".long-hand");
     this.second = clocks.filter(".second-hand");
-
+    this.dateField = date.filter(".analog-date");
+    this.dayField = date.filter(".analog-day");
 
     this.secondShifter = this.spawnShifter(60, 5);
     this.hourShifter = this.spawnShifter(12, 10);
-
+    this.symbols = this.spawnSymbols();
+    this.switchSymbols('signRoman');
   }
 
-  spawnShifter(n, long = 10) {
-    let lineData = [];
+  // create time divider around clock
+  spawnShifter(n, length = 10) {
+    const lineData = [];
     for (let i = 0; i < n; i++) {
       lineData.push({
-        x1: this.center + this.range - long,
+        x1: this.center + this.range - length,
         y1: this.center,
         x2: this.center + this.range,
         y2: this.center,
         "style": "stroke: #000",
-        'transform': 'rotate(' + (i * 360 / n - 90) + ' 150 150)'
+        "transform": "rotate(" + (i * 360 / n - 90) + " 150 150)"
       });
     }
-    let lines = this.svgContainer.selectAll()
+    const lines = this.svgContainer.selectAll()
       .data(lineData)
       .enter()
       .append("line");
 
-    this.d3Service.updateProperties(lineData, lines);
+    d3Service.updateProperties(lineData, lines);
     return lines;
   }
 
-  rotate(el, angle, max, duration) {
+  // create symbols around clock
+  spawnSymbols() {
+    const symbolData = [];
+    for (let i = 0; i < 12; i++) {
+      let angle = i / 6 * Math.PI - 45;
+      let x = (this.range + this.fontSize) * Math.cos(angle);
+      let y = (this.range + this.fontSize) * Math.sin(angle);
+      symbolData.push({
+        x: this.center,
+        y: this.center,
+        "font-size": this.fontSize + "px",
+        "class": "digital-number",
+        "text-anchor": "middle",
+        "fill": "gray",
+        "transform": `translate(${x}, ${y})`,
+        "dominant-baseline": "central",
+        "signArabic": this.symbolsSign.arabic[i],
+        "signRoman": this.symbolsSign.roman[i]
+      });
+    }
+    const symbols = this.svgContainer.selectAll()
+      .data(symbolData)
+      .enter()
+      .append("text");
+
+    d3Service.updateProperties(symbolData, symbols);
+    return symbols;
+  }
+
+  // switch visible type of symbols
+  switchSymbols(type) {
+    this.symbols.text(function(d) {
+      return d[type];
+    });
+  }
+
+  // rotate element to n fragment around circle with max fragments
+  rotate(el, n, max, duration) {
     el.transition().duration(duration)
       .attrTween("transform", () => {
-        angle = (angle / max * 360) + 180;
+        const angle = (n / max * 360) + 180;
         const i = d3.interpolate(angle, angle + 360 / max);
-        return (t) => {
-          return "rotate(" + i(t) + ",150,150)";
-        };
+        return (t) => "rotate(" + i(t) + ",150,150)";
       });
   }
 
+  // manage one tick (1 sec) of working clock
   tick() {
     const time = this.getTime();
     if (time.second != this.lastTime.second) {
@@ -112,12 +188,12 @@ export default class Analog extends Clock {
       this.rotate(this.second, time.second, 60, 20000);
       this.rotate(this.long, (time.minute * 60 + time.second), 3600, 5);
       this.rotate(this.short, (time.hour * 60 + time.minute), 720, 5);
+      const fullDate = this.getShortDate(this.lastDate);
+      if (fullDate) {
+        this.lastDate = fullDate.actual;
+        this.dateField.text(fullDate.dayDateText);
+        this.dayField.text(fullDate.dayNameText);
+      }
     }
-  }
-
-  render() {
-    this.run(() => {
-      this.tick()
-    });
   }
 }
--- app/scripts/digital.js
+++ app/scripts/digital.js
@@ -1,13 +1,14 @@
-import Clock from './clock';
+import Clock from "./clock";
 import * as d3 from "d3";
-import d3Service from './d3Service';
+import d3Service from "./d3Service";
 
 export default class Digital extends Clock {
 
   constructor() {
     super();
-    this.d3Service = new d3Service();
 
+    const svgWidth = 300;
+    const svgHeight = 300;
     this.width = 300;
     this.height = 150;
     this.fontSize = 80;
@@ -22,75 +23,98 @@ export default class Digital extends Clock {
     }];
 
     const numberData = [{
-      x: this.width / 2 - this.fontSize / 4,
+      x: this.width / 2 - this.fontSize / 2,
       y: this.height / 2 + this.fontSize / 4,
       "font-size": this.fontSize + "px",
-      "class": 'digital-number digital-hour',
+      "class": "digital-number digital-hour",
       "text-anchor": "end"
-    }, {
-      x: this.width / 2 + this.fontSize / 4,
-      y: this.height / 2 + this.fontSize / 4,
-      "font-size": this.fontSize + "px",
-      "class": 'digital-number digital-minute',
-      "text-anchor": "start"
     }, {
       x: this.width / 2,
       y: this.height / 2 + this.fontSize / 4,
       "font-size": this.fontSize + "px",
-      "class": 'digital-number digital-divider',
+      "class": "digital-number digital-minute",
+      "text-anchor": "start"
+    }, {
+      x: this.width / 2 - this.fontSize / 4,
+      y: this.height / 2 + this.fontSize / 4,
+      "font-size": this.fontSize + "px",
+      "class": "digital-number digital-divider",
       "text-anchor": "middle"
     }, {
-      x: this.width - this.fontSize / 8,
-      y: this.height - this.fontSize / 8,
+      x: this.width - this.fontSize / 6,
+      y: this.height / 2 + this.fontSize / 4,
       "font-size": this.fontSize / 2 + "px",
-      "class": 'digital-number digital-second',
+      "class": "digital-number digital-second",
       "text-anchor": "end"
-    }]
+    }, {
+      x: this.width - this.fontSize / 6,
+      y: this.height - this.fontSize / 8,
+      "font-size": this.fontSize / 3 + "px",
+      "class": "digital-number digital-msecond",
+      "text-anchor": "end"
+    }, {
+      x: this.fontSize / 2,
+      y: this.height - this.fontSize / 8,
+      "font-size": this.fontSize / 5 + "px",
+      "class": "digital-number digital-date",
+      "text-anchor": "start"
+    }];
 
-    let svgContainer = d3.select("div.clock-digital-svg").append("svg")
-      .attr("width", 300)
-      .attr("height", 300);
+    const svgContainer = d3.select("div.clock-digital-svg").append("svg")
+      .attr("width", svgWidth)
+      .attr("height", svgHeight);
 
-    let border = svgContainer.selectAll("rect")
+    const border = svgContainer.selectAll("rect")
       .data(borderData)
       .enter()
       .append("rect");
-    this.d3Service.updateProperties(borderData, border);
+    d3Service.updateProperties(borderData, border);
 
-    let textLabel = svgContainer.selectAll("text")
+    const textLabel = svgContainer.selectAll("text")
       .data(numberData)
       .enter()
       .append("text");
-    this.d3Service.updateProperties(numberData, textLabel);
+    d3Service.updateProperties(numberData, textLabel);
 
     this.digitalHour = textLabel.filter(".digital-hour");
     this.digitalMinute = textLabel.filter(".digital-minute");
     this.digitalDivider = textLabel.filter(".digital-divider");
     this.digitalDivider.text(":");
     this.digitalSecond = textLabel.filter(".digital-second");
-
-    this.lastTime = {};
+    this.digitalMsecond = textLabel.filter(".digital-msecond");
+    this.digitalDate = textLabel.filter(".digital-date");
   }
 
-  toTimer(time) {
+  // render miliseconds counter
+  updateMiliseconds(ms) {
+    setTimeout(() => {
+      this.digitalMsecond.text(ms);
+      ms += 10;
+      if (ms < 1000)
+        this.updateMiliseconds(ms);
+    }, 10);
+  }
+
+  // change format of time to minimum double numeric
+  toTimerPrefix(time) {
     return time < 10 ? "0" + time : time;
   }
 
+  // manage one tick (1 sec) of working clock
   tick() {
     const time = this.getTime();
     if (time.second != this.lastTime.second) {
       time.hour = this.timeMode ? time.hour : (time.hour > 12 ? (time.hour - 12) : time.hour)
-      this.digitalHour.text(this.toTimer(time.hour));
-      this.digitalMinute.text(this.toTimer(time.minute));
-      this.digitalSecond.text(this.toTimer(time.second));
+      this.digitalHour.text(this.toTimerPrefix(time.hour));
+      this.digitalMinute.text(this.toTimerPrefix(time.minute));
+      this.digitalSecond.text(this.toTimerPrefix(time.second));
+      const fullDate = this.getFullDate(this.lastDate);
+      if (fullDate) {
+        this.lastDate = fullDate.actual;
+        this.digitalDate.text(fullDate.dayNameText + ", " + fullDate.dayDateText);
+      }
       this.lastTime.second = time.second;
+      this.updateMiliseconds(0);
     }
   }
-
-  render() {
-    this.run(() => {
-      this.tick()
-    });
-  }
-
 }
